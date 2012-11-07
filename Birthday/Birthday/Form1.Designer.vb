e123ea6d6b324854c8e3d41e6bfd04a3aaf3de82
elfinkind <chris.kelley@twcny.rr.com>
2012-11-07 15:39:51 -0500

    file commit

---
+++ Birthday/Birthday/Form1.Designer.vb
@@ -0,0 +1,264 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip
+        Me.MenuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.CalculateToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.ResetToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.CloseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.HowToUseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.YearLabel = New System.Windows.Forms.Label
+        Me.YearComboBox = New System.Windows.Forms.ComboBox
+        Me.MonthLabel = New System.Windows.Forms.Label
+        Me.MonthComboBox = New System.Windows.Forms.ComboBox
+        Me.DayLabel = New System.Windows.Forms.Label
+        Me.DayComboBox = New System.Windows.Forms.ComboBox
+        Me.CalculateButton = New System.Windows.Forms.Button
+        Me.ResetButton = New System.Windows.Forms.Button
+        Me.CloseButton = New System.Windows.Forms.Button
+        Me.OutputLabel1 = New System.Windows.Forms.Label
+        Me.OutputLabel2 = New System.Windows.Forms.Label
+        Me.OutputLabel3 = New System.Windows.Forms.Label
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(7, 2, 0, 2)
+        Me.MenuStrip1.Size = New System.Drawing.Size(482, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MenuToolStripMenuItem
+        '
+        Me.MenuToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CalculateToolStripMenuItem, Me.ResetToolStripMenuItem, Me.CloseToolStripMenuItem})
+        Me.MenuToolStripMenuItem.Name = "MenuToolStripMenuItem"
+        Me.MenuToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.MenuToolStripMenuItem.Text = "Menu"
+        '
+        'CalculateToolStripMenuItem
+        '
+        Me.CalculateToolStripMenuItem.Name = "CalculateToolStripMenuItem"
+        Me.CalculateToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.CalculateToolStripMenuItem.Text = "Calculate"
+        '
+        'ResetToolStripMenuItem
+        '
+        Me.ResetToolStripMenuItem.Name = "ResetToolStripMenuItem"
+        Me.ResetToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ResetToolStripMenuItem.Text = "Reset"
+        '
+        'CloseToolStripMenuItem
+        '
+        Me.CloseToolStripMenuItem.Name = "CloseToolStripMenuItem"
+        Me.CloseToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.CloseToolStripMenuItem.Text = "Close"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.HowToUseToolStripMenuItem, Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'HowToUseToolStripMenuItem
+        '
+        Me.HowToUseToolStripMenuItem.Name = "HowToUseToolStripMenuItem"
+        Me.HowToUseToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.HowToUseToolStripMenuItem.Text = "How to use"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'YearLabel
+        '
+        Me.YearLabel.AutoSize = True
+        Me.YearLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.YearLabel.Location = New System.Drawing.Point(12, 33)
+        Me.YearLabel.Name = "YearLabel"
+        Me.YearLabel.Size = New System.Drawing.Size(155, 20)
+        Me.YearLabel.TabIndex = 0
+        Me.YearLabel.Text = "Enter your birth year:"
+        '
+        'YearComboBox
+        '
+        Me.YearComboBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.YearComboBox.FormattingEnabled = True
+        Me.YearComboBox.Location = New System.Drawing.Point(12, 61)
+        Me.YearComboBox.Name = "YearComboBox"
+        Me.YearComboBox.Size = New System.Drawing.Size(121, 28)
+        Me.YearComboBox.TabIndex = 1
+        '
+        'MonthLabel
+        '
+        Me.MonthLabel.AutoSize = True
+        Me.MonthLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MonthLabel.Location = New System.Drawing.Point(12, 97)
+        Me.MonthLabel.Name = "MonthLabel"
+        Me.MonthLabel.Size = New System.Drawing.Size(170, 20)
+        Me.MonthLabel.TabIndex = 0
+        Me.MonthLabel.Text = "Enter your birth month:"
+        '
+        'MonthComboBox
+        '
+        Me.MonthComboBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MonthComboBox.FormattingEnabled = True
+        Me.MonthComboBox.Items.AddRange(New Object() {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"})
+        Me.MonthComboBox.Location = New System.Drawing.Point(12, 125)
+        Me.MonthComboBox.Name = "MonthComboBox"
+        Me.MonthComboBox.Size = New System.Drawing.Size(121, 28)
+        Me.MonthComboBox.TabIndex = 2
+        '
+        'DayLabel
+        '
+        Me.DayLabel.AutoSize = True
+        Me.DayLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DayLabel.Location = New System.Drawing.Point(12, 161)
+        Me.DayLabel.Name = "DayLabel"
+        Me.DayLabel.Size = New System.Drawing.Size(150, 20)
+        Me.DayLabel.TabIndex = 0
+        Me.DayLabel.Text = "Enter your birth day:"
+        '
+        'DayComboBox
+        '
+        Me.DayComboBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DayComboBox.FormattingEnabled = True
+        Me.DayComboBox.Location = New System.Drawing.Point(12, 189)
+        Me.DayComboBox.Name = "DayComboBox"
+        Me.DayComboBox.Size = New System.Drawing.Size(121, 28)
+        Me.DayComboBox.TabIndex = 3
+        '
+        'CalculateButton
+        '
+        Me.CalculateButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CalculateButton.Location = New System.Drawing.Point(48, 225)
+        Me.CalculateButton.Name = "CalculateButton"
+        Me.CalculateButton.Size = New System.Drawing.Size(88, 30)
+        Me.CalculateButton.TabIndex = 4
+        Me.CalculateButton.Text = "Calculate"
+        Me.CalculateButton.UseVisualStyleBackColor = True
+        '
+        'ResetButton
+        '
+        Me.ResetButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ResetButton.Location = New System.Drawing.Point(197, 225)
+        Me.ResetButton.Name = "ResetButton"
+        Me.ResetButton.Size = New System.Drawing.Size(88, 30)
+        Me.ResetButton.TabIndex = 5
+        Me.ResetButton.Text = "Reset"
+        Me.ResetButton.UseVisualStyleBackColor = True
+        '
+        'CloseButton
+        '
+        Me.CloseButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CloseButton.Location = New System.Drawing.Point(346, 225)
+        Me.CloseButton.Name = "CloseButton"
+        Me.CloseButton.Size = New System.Drawing.Size(88, 30)
+        Me.CloseButton.TabIndex = 6
+        Me.CloseButton.Text = "Close"
+        Me.CloseButton.UseVisualStyleBackColor = True
+        '
+        'OutputLabel1
+        '
+        Me.OutputLabel1.AutoSize = True
+        Me.OutputLabel1.Location = New System.Drawing.Point(194, 67)
+        Me.OutputLabel1.Name = "OutputLabel1"
+        Me.OutputLabel1.Size = New System.Drawing.Size(0, 15)
+        Me.OutputLabel1.TabIndex = 7
+        '
+        'OutputLabel2
+        '
+        Me.OutputLabel2.AutoSize = True
+        Me.OutputLabel2.Location = New System.Drawing.Point(194, 99)
+        Me.OutputLabel2.Name = "OutputLabel2"
+        Me.OutputLabel2.Size = New System.Drawing.Size(0, 15)
+        Me.OutputLabel2.TabIndex = 8
+        '
+        'OutputLabel3
+        '
+        Me.OutputLabel3.AutoSize = True
+        Me.OutputLabel3.Location = New System.Drawing.Point(194, 131)
+        Me.OutputLabel3.Name = "OutputLabel3"
+        Me.OutputLabel3.Size = New System.Drawing.Size(0, 15)
+        Me.OutputLabel3.TabIndex = 9
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(482, 267)
+        Me.Controls.Add(Me.OutputLabel3)
+        Me.Controls.Add(Me.OutputLabel2)
+        Me.Controls.Add(Me.OutputLabel1)
+        Me.Controls.Add(Me.CloseButton)
+        Me.Controls.Add(Me.ResetButton)
+        Me.Controls.Add(Me.CalculateButton)
+        Me.Controls.Add(Me.DayComboBox)
+        Me.Controls.Add(Me.DayLabel)
+        Me.Controls.Add(Me.MonthComboBox)
+        Me.Controls.Add(Me.MonthLabel)
+        Me.Controls.Add(Me.YearComboBox)
+        Me.Controls.Add(Me.YearLabel)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Form1"
+        Me.Text = "On what day were you born?"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents MenuToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CalculateToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CloseToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HowToUseToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents YearLabel As System.Windows.Forms.Label
+    Friend WithEvents YearComboBox As System.Windows.Forms.ComboBox
+    Friend WithEvents MonthLabel As System.Windows.Forms.Label
+    Friend WithEvents MonthComboBox As System.Windows.Forms.ComboBox
+    Friend WithEvents DayLabel As System.Windows.Forms.Label
+    Friend WithEvents DayComboBox As System.Windows.Forms.ComboBox
+    Friend WithEvents CalculateButton As System.Windows.Forms.Button
+    Friend WithEvents ResetButton As System.Windows.Forms.Button
+    Friend WithEvents CloseButton As System.Windows.Forms.Button
+    Friend WithEvents OutputLabel1 As System.Windows.Forms.Label
+    Friend WithEvents OutputLabel2 As System.Windows.Forms.Label
+    Friend WithEvents ResetToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents OutputLabel3 As System.Windows.Forms.Label
+
+End Class
---
+++ Birthday/Birthday/Form1.vb
@@ -0,0 +1,269 @@
+﻿Public Class Form1
+
+    Dim YearArray(199) As Integer
+    Dim YearCounter As Integer = 0
+    Dim FullYear, ModYear, LeapYear, MonthCounter, DayCounter As Integer
+    Dim StepOne, StepTwo, StepThree, StepFour, StepFive, StepSix, StepSeven, StepEight, StepNine As Double
+    Dim Day As String
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+
+
+        For a = 1900 To 2099
+
+            YearArray(YearCounter) = a
+            YearComboBox.Items.Add(a)
+
+            YearCounter += 1
+
+        Next
+
+    End Sub
+
+    Private Sub MonthComboBox_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MonthComboBox.SelectedIndexChanged
+
+        DayComboBox.SelectedIndex = -1
+        DayComboBox.Items.Clear()
+
+        FullYear = CInt(YearComboBox.SelectedItem)
+        ModYear = FullYear Mod 100
+
+        If FullYear Mod 4 = 0 Then
+
+            If FullYear Mod 100 = 0 Then
+
+                If FullYear Mod 400 = 0 Then
+
+                    LeapYear = 1
+
+                End If
+
+            Else
+
+                LeapYear = 0
+
+            End If
+
+            LeapYear = 1
+
+        Else
+
+            LeapYear = 0
+
+        End If
+
+        If LeapYear = 1 And MonthComboBox.SelectedIndex = 1 Then
+
+            DayCounter = 29
+
+        ElseIf LeapYear = 0 And MonthComboBox.SelectedIndex = 1 Then
+
+            DayCounter = 28
+
+        Else
+
+            Select Case MonthComboBox.SelectedIndex
+
+                Case 0, 2, 4, 6, 7, 9, 11
+
+                    DayCounter = 31
+
+                Case 3, 5, 8, 10
+
+                    DayCounter = 30
+
+            End Select
+
+        End If
+
+        For a = 1 To DayCounter
+
+            DayComboBox.Items.Add(a.ToString)
+
+        Next
+
+    End Sub
+
+    Private Sub CalculateButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CalculateButton.Click, CalculateToolStripMenuItem.Click
+
+        If YearComboBox.SelectedIndex = -1 Or MonthComboBox.SelectedIndex = -1 Or DayComboBox.SelectedIndex = -1 Then
+
+            ErrorMessage()
+
+            If YearComboBox.SelectedIndex = -1 Then
+
+                YearLabel.ForeColor = Color.Red
+                YearLabel.Font = New System.Drawing.Font(YearLabel.Font, FontStyle.Bold)
+
+            Else
+
+                YearLabel.Font = New System.Drawing.Font(YearLabel.Font, FontStyle.Regular)
+                YearLabel.ForeColor = Color.Black
+
+            End If
+
+
+            If MonthComboBox.SelectedIndex = -1 Then
+
+                MonthLabel.ForeColor = Color.Red
+                MonthLabel.Font = New System.Drawing.Font(MonthLabel.Font, FontStyle.Bold)
+
+            Else
+
+                MonthLabel.Font = New System.Drawing.Font(MonthLabel.Font, FontStyle.Regular)
+                MonthLabel.ForeColor = Color.Black
+
+            End If
+
+
+            If DayComboBox.SelectedIndex = -1 Then
+
+                DayLabel.ForeColor = Color.Red
+                DayLabel.Font = New System.Drawing.Font(DayLabel.Font, FontStyle.Bold)
+
+            Else
+
+                DayLabel.Font = New System.Drawing.Font(DayLabel.Font, FontStyle.Regular)
+                DayLabel.ForeColor = Color.Black
+
+            End If
+
+        Else
+
+            Dim MonthCode As Integer
+
+            Select Case MonthComboBox.SelectedIndex
+
+                Case 0, 9
+                    MonthCode = 1
+                Case 1, 2, 10
+                    MonthCode = 4
+                Case 3, 6
+                    MonthCode = 0
+                Case 4
+                    MonthCode = 2
+                Case 5
+                    MonthCode = 5
+                Case 7
+                    MonthCode = 3
+                Case 8, 11
+                    MonthCode = 6
+
+            End Select
+
+            StepOne = ModYear * 5
+            StepTwo = StepOne / 4
+            StepThree = StepTwo - (StepTwo Mod 1)
+            StepFour = StepThree + MonthCode
+            StepFive = StepFour + CInt(DayComboBox.SelectedItem)
+            StepSix = StepFive / 7
+            StepSeven = StepSix * 10
+            StepEight = StepSeven - (StepSeven Mod 1)
+            StepNine = StepEight Mod 10
+
+            If LeapYear = 1 Then
+
+                If MonthComboBox.SelectedIndex = 1 Or MonthComboBox.SelectedIndex = 1 Then
+
+                    StepNine = StepNine - 1
+
+                    If StepNine < 0 Then
+
+                        StepNine = 1
+
+                    End If
+
+                End If
+
+            End If
+
+            Select Case StepNine
+
+                Case 1
+                    Day = "Sunday"
+                Case 2
+                    Day = "Monday"
+                Case 4
+                    Day = "Tuesday"
+                Case 5
+                    Day = "Wednesday"
+                Case 7
+                    Day = "Thursday"
+                Case 8
+                    Day = "Friday"
+                Case 0
+                    Day = "Saturday"
+
+            End Select
+
+            OutputLabel1.Text = "You were born on " & (MonthComboBox.SelectedIndex + 1).ToString & "-" & DayComboBox.SelectedItem.ToString & "-" & YearComboBox.SelectedItem.ToString & "."
+
+            If LeapYear = 1 Then
+
+                OutputLabel2.Text = FullYear & " was a leap year."
+
+            ElseIf LeapYear = 0 Then
+
+                OutputLabel2.Text = FullYear & " was not a leap year"
+
+            End If
+            OutputLabel3.Text = "You were born on a " & Day & "."
+
+        End If
+
+    End Sub
+
+    Private Sub ErrorMessage()
+
+        MessageBox.Show("You must supply me with year, month, and day, or I can't tell you the day of the week on which you were born." & Environment.NewLine & Environment.NewLine & "Please try again.", "Blank Fields", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+    End Sub
+
+    Private Sub ResetButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ResetButton.Click, ResetToolStripMenuItem.Click
+
+        Reset()
+
+    End Sub
+
+    Private Sub Reset()
+
+        YearLabel.Font = New System.Drawing.Font(YearLabel.Font, FontStyle.Regular)
+        YearLabel.ForeColor = Color.Black
+
+        MonthLabel.Font = New System.Drawing.Font(MonthLabel.Font, FontStyle.Regular)
+        MonthLabel.ForeColor = Color.Black
+
+        DayLabel.Font = New System.Drawing.Font(DayLabel.Font, FontStyle.Regular)
+        DayLabel.ForeColor = Color.Black
+
+        YearComboBox.SelectedIndex = -1
+
+        MonthComboBox.SelectedIndex = -1
+
+        DayComboBox.SelectedIndex = -1
+        DayComboBox.Items.Clear()
+
+        OutputLabel1.ResetText()
+        OutputLabel2.ResetText()
+
+    End Sub
+
+    Private Sub CloseButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CloseButton.Click
+
+        Me.Close()
+
+    End Sub
+
+    Private Sub CloseToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CloseToolStripMenuItem.Click
+
+        Me.Close()
+
+    End Sub
+
+    Private Sub HowToUseToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles HowToUseToolStripMenuItem.Click
+
+        MessageBox.Show("This application will tell you the day of the week on a particular date fell, falls, or will fall, from 1900 through 2099. Leap years are accounted for, so just enter data as needed." & Environment.NewLine & Environment.NewLine & "Simply choose your birth year, month, and day from the drop down menus, and then press the Calculate button." & Environment.NewLine & Environment.NewLine & "The application will then display the birthday you entered and the day of the week for that date." & Environment.NewLine & Environment.NewLine & "To try again, you can either immediately choose new options from the drop down menus, or you can press the Reset button to clear the fields and then choose new information." & Environment.NewLine & Environment.NewLine & "To exit the application, click the Close button.", "How to use", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+    End Sub
+End Class
